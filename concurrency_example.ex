a3eb79c1b99094028796a9eaab67f23560b62449
Aivyss <hklee6417@gmail.com>
2025-07-05 01:43:37 +0900

    concurrency1

---
+++ concurrency_example.ex
@@ -0,0 +1,48 @@
+IO.puts "===spawn==="
+defmodule ConcurrencyExample do
+  def add do
+    receive do
+      [a, b] ->
+        if is_integer(a) and is_integer(b) do
+          IO.puts "#{a} + #{b} = #{a + b}"
+        else
+          IO.puts "invalid arguments"
+        end
+      _ ->
+        IO.puts "invalid arguments"
+    end
+
+    add() # PIDを再利用するため、わざと無限ループさせた
+  end
+end
+spawn(ConcurrencyExample, :add, [])
+
+IO.puts "===send message==="
+pid = spawn(ConcurrencyExample, :add, []) |> IO.inspect
+send pid, [] # invalid arguments
+send pid, {1, 2} # invalid arguments
+send pid, [1, 2] # 1 + 2 = 3
+:timer.sleep(1000)
+
+IO.puts "===spawn link==="
+defmodule ConcurrencyExample2 do
+  def explode, do: exit(:kaboooom)
+
+  @doc """
+  プロセス終了を受信してハンドリングする
+  """
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(ConcurrencyExample2, :explode, []) |> IO.inspect # プロセスの終了を受信することができる
+
+    receive do
+      {:EXIT, from_pid, reason} ->
+        IO.puts "Process #{inspect from_pid} exited with reason #{inspect reason}"
+    end
+  end
+end
+
+# spawn(ConcurrencyExample2, :explode, []) |> IO.inspect # プロセスが終了されたかがわからない
+# spawn_link(ConcurrencyExample2, :explode, []) |> IO.inspect # プロセスの終了を受信することができる
+
+ConcurrencyExample2.run()
